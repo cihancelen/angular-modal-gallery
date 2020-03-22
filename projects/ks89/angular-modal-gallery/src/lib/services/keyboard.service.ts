592197ff7b0ca20fb22ca5e5041dde44b42393c4
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2020-03-22 23:43:59 +0100

    feat(): remove Module forRoot and upgrade keyboard service reading config with config.service #183

--- projects/ks89/angular-modal-gallery/src/lib/services/keyboard.service.ts
+++ projects/ks89/angular-modal-gallery/src/lib/services/keyboard.service.ts
@@ -22,38 +22,23 @@
  SOFTWARE.
  */
 
-import { Inject, Injectable, InjectionToken } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import { KeyboardServiceConfig } from '../model/keyboard-service-config.interface';
-
-export const KEYBOARD_CONFIGURATION = new InjectionToken<KeyboardServiceConfig>('KEYBOARD_CONFIGURATION');
+import { LibConfig } from './config.service';
 
 /**
  * Service to intercept ctrl+s (or cmd+s on macOS) using a third-party library, called Mousetrap.
  */
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class KeyboardService {
   /**
    * Private Mousetrap variable to store the instance.
    */
   private mousetrap: MousetrapInstance;
-  /**
-   * Private variable to store shortcuts as either Array or string.
-   */
-  private shortcuts: Array<string> | string;
 
-  /**
-   * Constructor of `KeyboardService` to init `mousetrap` and `shortcuts` private variables.
-   * @param KeyboardServiceConfig config object received by the `forRoot()` function to init custom shortcuts
-   */
-  constructor(@Inject(KEYBOARD_CONFIGURATION) private config: KeyboardServiceConfig) {
-    // this.config is always defined, because forced by forRoot inside the module
-    // when empty, it's simply an empty object: {}
-
-    this.shortcuts = this.config && this.config.shortcuts ? this.config.shortcuts : ['ctrl+s', 'meta+s'];
-
+  init(config: LibConfig) {
     // temporary workaround to fix this issue: https://github.com/Ks89/angular-modal-gallery/issues/142
-    if (this.config && !this.config.disableSsrWorkaround) {
+    if (!config.keyboardServiceConfig.disableSsrWorkaround) {
       // To prevent issues with angular-universal on server-side
       if (typeof window !== 'undefined') {
         require('mousetrap');
@@ -66,12 +51,12 @@ export class KeyboardService {
    * Method to add a lister for ctrl+s/cmd+s keyboard events.
    * @param (e: ExtendedKeyboardEvent, combo: string) => any onBind callback function to add shortcuts
    */
-  add(onBind: (e: ExtendedKeyboardEvent, combo: string) => any) {
+  add(onBind: (e: ExtendedKeyboardEvent, combo: string) => any, config: LibConfig) {
     // temporary workaround to fix this issue: https://github.com/Ks89/angular-modal-gallery/issues/142
-    if (this.config && !this.config.disableSsrWorkaround) {
+    if (!config.keyboardServiceConfig.disableSsrWorkaround) {
       // To prevent issues with angular-universal on server-side
       if (typeof window !== 'undefined') {
-        this.mousetrap.bind(this.shortcuts, (event: KeyboardEvent, combo: string) => {
+        this.mousetrap.bind(config.keyboardServiceConfig.shortcuts, (event: KeyboardEvent, combo: string) => {
           if (event.preventDefault) {
             event.preventDefault();
           } else {
@@ -88,9 +73,9 @@ export class KeyboardService {
    * Method to reset all listeners. Please, call this function when needed
    * to free resources ad prevent leaks.
    */
-  reset() {
+  reset(config: LibConfig) {
     // temporary workaround to fix this issue: https://github.com/Ks89/angular-modal-gallery/issues/142
-    if (this.config && !this.config.disableSsrWorkaround) {
+    if (!config.keyboardServiceConfig.disableSsrWorkaround) {
       // To prevent issues with angular-universal on server-side
       if (typeof window !== 'undefined') {
         this.mousetrap.reset();
